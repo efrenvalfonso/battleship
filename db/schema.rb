9d357d34b8d0fc977c7ea2612db981a466c48744
Efren Vila Alfonso <efrenvalfonso@gmail.com>
2020-06-16 15:29:27 -0400

    updated schema with db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_16_190106) do
+ActiveRecord::Schema.define(version: 2020_06_16_192722) do
 
   create_table "players", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["name"], name: "index_players_on_name", unique: true
   end
 
 end
